40e97bc51713d0cd6b90a858d38d64ab5175d72d
Luis Otávio Miranda Amaro <luis.amaro@agv.intranet>
2024-11-08 17:45:58 -0300

    atualizando components

--- projetopim/src/App.js
+++ projetopim/src/App.js
@@ -3,16 +3,15 @@ import './App.css'; // Importando o arquivo CSS principal do App
 import ImagemFundo from './components/Imagem/ImagemFundo'; // Importando o componente ImagemFundo
 import Nav from './components/Nav/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Certifique-se de importar o CSS do Bootstrap
+import Card from './components/Card/Card';
 
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-      <ImagemFundo/>
-      
-      
-      
+      <Card/>
+
     </div>
   );
 }
---
+++ projetopim/src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+import './Card.css'
+
+export default function Card () {
+    return (
+        <div className='flex-container'>
+            <div class="card">
+               <h1> Meu card</h1> 
+               <p> </p>
+            </div>
+          
+        </div>
+        
+    )
+}
